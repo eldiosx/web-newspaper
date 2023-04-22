da7db0d4ca7fc062f8ffde820b17de6ccdd3d4b8
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-04-22 17:21:01 +0200

    minorFix

--- buttons/index.js
+++ buttons/index.js
@@ -1,7 +1,7 @@
 var button1 = document.getElementById("button1");
 var audio1 = document.getElementById("audio1");
 
-button1.addEventListener("mousedown", function() {
+button1.addEventListener("mousedown", function () {
   audio1.currentTime = 0;
   audio1.play();
 });
